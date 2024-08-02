643ccca91ed58765f80f81e5d58772ea46324f7d
edunyx <edu.elias.pe@gmail.com>
2024-08-02 03:14:47 -0500

    Actualizando botones

---
+++ encrypter.js
@@ -0,0 +1,59 @@
+
+
+
+
+btn_inicial_oculto();
+
+
+function btn_inicial_oculto(){
+    document.getElementById('copiar').style.visibility='hidden';
+
+}
+
+
+function clean(){
+
+
+let limpiartextos =document.querySelector('#texto').value='';
+
+}
+
+
+
+function encriptar(){
+
+
+ document.getElementById('copiar').style.visibility='visible'
+ 
+
+ hiddenelementos();
+
+}
+
+
+function hiddenelementos(){
+
+
+document.getElementById('img').style.visibility='hidden';
+document.getElementById('txt1').style.visibility='hidden';
+document.getElementById('txt2').style.visibility='hidden';
+
+}
+
+function copiar(){
+    
+   /* let copiartexto =document.querySelector('#texto2').value='';*/
+   
+
+ 
+    texto_copiado();
+
+}
+
+function texto_copiado(){
+   
+alert("hola")
+}
+
+
+
